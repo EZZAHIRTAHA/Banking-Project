863661645fa275d384b2790b9fbc9ce7fc31e47d
Ezzahir Taha <taha1.ezzahir@gmail.com>
2023-06-09 11:39:32 +0200

    Added and deleted features

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,14 @@ import { close, logo, menu } from "../assets";
 import { navLinks } from "../constants";
 
 const Navbar = () => {
+
+  
   const [active, setActive] = useState("Home");
   const [toggle, setToggle] = useState(false);
 
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
-      <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
+      <img src={`./logo1.png`} alt="hoobank" className="w-[10%] rounded-full border-[1px] border-[#51d7e0]" />
 
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
