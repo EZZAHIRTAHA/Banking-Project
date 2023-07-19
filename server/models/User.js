f24b36663680257e7720c0221c32f679f0128b93
Ezzahir Taha <taha1.ezzahir@gmail.com>
2023-07-19 22:12:17 +0200

    Feat: User model

---
+++ server/models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose")
+
+const userSchema = mongoose.Schema({
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+      validate: {
+        validator: function (value) {
+          const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+          return emailRegex.test(value);
+        },
+        message: 'Invalid email address',
+      },
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+      minlength: [8, 'Password must be at least 8 characters long'],
+    },
+  }, { timestamps: true });
+
+  userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+      next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  });
+
+
+
+export default mongoose.model("User", userSchema)
